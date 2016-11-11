02b60fbf04f0ac4d17ef97b44f5f83f8c52f7aa2
Christopher Kiel <no-fi@users.noreply.github.com>
2016-11-10 23:31:50 -0600

    Adds a test for SampleTagList component

--- client/components/SampleTagList.jsx
+++ client/components/SampleTagList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function SampleTagList (props) {
   return (
-    <div className='tag-list'>
+    <div className='sample-tag-list'>
       <ul>
-        { props.tags.map(tag => <li key={tag}>{ tag }</li>) }
+        { props.tags.map(tag => <li className='sample-tag' key={tag}>{ tag }</li>) }
       </ul>
     </div>
   );
